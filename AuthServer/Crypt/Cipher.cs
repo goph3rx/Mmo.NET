5ce60a4f75f9f22ad7efd7ca865e47f1240a04de
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-03-27 14:35:25 +0000

    Traffic encryption

---
+++ AuthServer/Crypt/Cipher.cs
@@ -0,0 +1,38 @@
+﻿namespace Mmo.AuthServer.Crypt;
+
+/// <summary>
+/// Encryption and decryption of traffic.
+/// </summary>
+public class Cipher
+{
+    /// <summary>
+    /// Size of a single encryption/decryption block.
+    /// </summary>
+    public const int BlockSize = Blowfish.BlockSize;
+
+    private readonly Blowfish decrypt;
+    private readonly Blowfish encrypt;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Cipher"/> class.
+    /// </summary>
+    /// <param name="key">Encryption key (uses initial key if unspecified).</param>
+    public Cipher(byte[]? key = null)
+    {
+        key ??= new byte[] { 0x6B, 0x60, 0xCB, 0x5B, 0x82, 0xCE, 0x90, 0xB1, 0xCC, 0x2B, 0x6C, 0x55, 0x6C, 0x6C, 0x6C, 0x6C };
+        this.encrypt = new Blowfish(true, key);
+        this.decrypt = new Blowfish(false, key);
+    }
+
+    /// <summary>
+    /// Encrypt the buffer in place.
+    /// </summary>
+    /// <param name="buffer">Buffer with data (must be padded).</param>
+    public void Encrypt(Span<byte> buffer)
+    {
+        for (var offset = 0; offset < buffer.Length; offset += BlockSize)
+        {
+            this.encrypt.ProcessBlock(buffer[offset..], buffer[offset..]);
+        }
+    }
+}
---
+++ Tests/Crypt/AuthCipherTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mmo.AuthServer.Crypt;
+
+namespace Mmo.Tests.Crypt;
+
+[TestClass]
+public class AuthCipherTest
+{
+    [TestMethod]
+    public void Encrypt()
+    {
+        // Given
+        var buffer = Convert.FromHexString("0102030405060708090A0B0C0D0E0F10");
+        var cipher = new Cipher();
+
+        // When
+        cipher.Encrypt(buffer);
+
+        // Then
+        Assert.AreEqual("569A83DF25C8816A99368A84726F4FD7", Convert.ToHexString(buffer));
+    }
+}
+
