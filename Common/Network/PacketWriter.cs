81fc913ac5d61077211e858c4e16b4d273ea9f79
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-03-27 11:27:30 +0000

    Base functionality for writing packets

---
+++ Common/Network/PacketWriter.cs
@@ -0,0 +1,85 @@
+﻿using System.Buffers.Binary;
+
+namespace Mmo.Common.Network;
+
+/// <summary>
+/// Provides primitives for serializing packets.
+/// </summary>
+public ref struct PacketWriter
+{
+    /// <summary>
+    /// Memory that we're writing to.
+    /// </summary>
+    private readonly Span<byte> memory;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PacketWriter"/> struct.
+    /// </summary>
+    /// <param name="memory">Underlying memory.</param>
+    public PacketWriter(Span<byte> memory)
+    {
+        this.memory = memory;
+
+        // Reset the memory
+        this.memory.Fill(0);
+    }
+
+    /// <summary>
+    /// Gets the length of the written data.
+    /// </summary>
+    public int Length { get; private set; } = 0;
+
+    /// <summary>
+    /// Skip a given number of bytes.
+    /// </summary>
+    /// <param name="length">How many bytes to skip.</param>
+    /// <exception cref="Exception">If skipping causes an overflow.</exception>
+    public void Skip(int length)
+    {
+        if (this.Length + length > this.memory.Length)
+        {
+            throw new ArgumentException("Memory overflow", nameof(length));
+        }
+
+        this.Length += length;
+    }
+
+    /// <summary>
+    /// Write a C value (1 byte).
+    /// </summary>
+    /// <param name="value">Value to write.</param>
+    public void WriteC(byte value)
+    {
+        this.memory[this.Length] = value;
+        this.Length++;
+    }
+
+    /// <summary>
+    /// Write a D value (4 bytes).
+    /// </summary>
+    /// <param name="value">Value to write.</param>
+    public void WriteD(int value)
+    {
+        BinaryPrimitives.WriteInt32LittleEndian(this.memory[this.Length..], value);
+        this.Length += 4;
+    }
+
+    /// <summary>
+    /// Write a B value.
+    /// </summary>
+    /// <param name="value">Value to write.</param>
+    public void WriteB(ReadOnlySpan<byte> value)
+    {
+        value.CopyTo(this.memory[this.Length..]);
+        this.Length += value.Length;
+    }
+
+    /// <summary>
+    /// Get the written data as a span.
+    /// </summary>
+    /// <returns>Written data as a span.</returns>
+    public readonly ReadOnlySpan<byte> AsSpan()
+    {
+        return this.memory[..this.Length];
+    }
+}
---
+++ Tests/Network/PacketWriterTest.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mmo.Common.Network;
+
+namespace Mmo.Tests.Network;
+
+[TestClass]
+public class PacketWriterTest
+{
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void SkipOverflow()
+    {
+        // Given
+        var writer = new PacketWriter();
+
+        // When/Then
+        writer.Skip(1);
+    }
+
+    [TestMethod]
+    public void Skip()
+    {
+        // Given
+        var memory = new byte[16];
+        var writer = new PacketWriter(memory);
+
+        // When
+        writer.Skip(8);
+
+        // Then
+        Assert.AreEqual("0000000000000000", Convert.ToHexString(writer.AsSpan()));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(IndexOutOfRangeException))]
+    public void WriteCOverflow()
+    {
+        // Given
+        var memory = Array.Empty<byte>();
+        var writer = new PacketWriter(memory);
+
+        // When/Then
+        writer.WriteC(0x7B);
+    }
+
+    [TestMethod]
+    public void WriteC()
+    {
+        // Given
+        var memory = new byte[1];
+        var writer = new PacketWriter(memory);
+
+        // When
+        writer.WriteC(0x7B);
+
+        // Then
+        Assert.AreEqual("7B", Convert.ToHexString(writer.AsSpan()));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void WriteDOverflow()
+    {
+        // Given
+        var memory = Array.Empty<byte>();
+        var writer = new PacketWriter(memory);
+
+        // When/Then
+        writer.WriteD(0x105C6A7B);
+    }
+
+    [TestMethod]
+    public void WriteD()
+    {
+        // Given
+        var memory = new byte[16];
+        var writer = new PacketWriter(memory);
+
+        // When
+        writer.WriteD(0x105C6A7B);
+
+        // Then
+        Assert.AreEqual("7B6A5C10", Convert.ToHexString(writer.AsSpan()));
+    }
+
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void WriteBOverflow()
+    {
+        // Given
+        var memory = Array.Empty<byte>();
+        var writer = new PacketWriter(memory);
+
+        // When/Then
+        writer.WriteB(new byte[] { 0 });
+    }
+
+    [TestMethod]
+    public void WriteB()
+    {
+        // Given
+        var memory = new byte[16];
+        var writer = new PacketWriter(memory);
+
+        // When
+        writer.WriteB(new byte[] { 1, 2, 3 });
+
+        // Then
+        Assert.AreEqual("010203", Convert.ToHexString(writer.AsSpan()));
+    }
+}
