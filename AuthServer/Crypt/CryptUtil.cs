1fd113298a3fa0250b2b69cc15f2c29b810392b3
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-03-27 11:31:49 +0000

    Scramble the modulus for username/password encryption

---
+++ AuthServer/Crypt/CryptUtil.cs
@@ -0,0 +1,34 @@
+﻿namespace Mmo.AuthServer.Crypt;
+
+/// <summary>
+/// Utilities for encryption.
+/// </summary>
+public static class CryptUtil
+{
+    /// <summary>
+    /// Scramble the modulus for username/password encryption.
+    /// </summary>
+    /// <param name="modulus">Modulus.</param>
+    public static void ScrambleModulus(byte[] modulus)
+    {
+        for (var i = 0; i < 4; i++)
+        {
+            (modulus[i], modulus[i + 77]) = (modulus[i + 77], modulus[i]);
+        }
+
+        for (var i = 0; i < 64; i++)
+        {
+            modulus[i] = (byte)(modulus[i] ^ modulus[i + 64]);
+        }
+
+        for (var i = 0; i < 4; i++)
+        {
+            modulus[i + 13] = (byte)(modulus[i + 13] ^ modulus[i + 52]);
+        }
+
+        for (var i = 0; i < 64; i++)
+        {
+            modulus[i + 64] = (byte)(modulus[i + 64] ^ modulus[i]);
+        }
+    }
+}
