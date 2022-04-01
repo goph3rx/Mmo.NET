915de52e1b16cc1a63919a7ff1c3922a3de722dd
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-04-01 08:39:56 +0000

    Basic implementation of a connection with the client (#10)

---
+++ AuthServer/IConnection.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using Mmo.AuthServer.Network;
+
+namespace Mmo.AuthServer;
+
+/// <summary>
+/// Interface for the client connection.
+/// </summary>
+public interface IConnection
+{
+    /// <summary>
+    /// Gets address of the client.
+    /// </summary>
+    EndPoint? RemoteEndPoint { get; }
+
+    /// <summary>
+    /// Send a message.
+    /// </summary>
+    /// <param name="message">Message.</param>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+    Task SendAsync(IServerMessage message);
+}
