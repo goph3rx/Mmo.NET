ef50ad513d8b91ad8f3fab578f35598204ecd0eb
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-04-01 09:11:17 +0000

    Basic client implementation (#11)

---
+++ Tests/Network/AuthClientTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mmo.AuthServer;
+using Mmo.AuthServer.Network;
+using Moq;
+
+namespace Mmo.Tests.Network;
+
+[TestClass]
+public class AuthClientTest
+{
+    private Mock<ILogger<Client>> logger;
+    private Mock<IConnection> connection;
+    private Client client;
+
+    public AuthClientTest()
+    {
+        this.logger = new Mock<ILogger<Client>>();
+        this.connection = new Mock<IConnection>();
+        this.client = new Client(this.connection.Object, this.logger.Object);
+    }
+
+    [TestMethod]
+    public async Task InitAsync()
+    {
+        // Given
+        this.connection.
+            Setup(x => x.SendAsync(It.Is<ServerInit>(message => message.Modulus.Length == 128 && message.CryptKey.Length == 16))).
+            Returns(Task.CompletedTask).
+            Verifiable();
+
+        // When
+        await this.client.InitAsync();
+
+        // Then
+        this.connection.Verify();
+    }
+}
