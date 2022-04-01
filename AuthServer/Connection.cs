fc801001e14c91888e9e97e3aafdeee3c14a01b0
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-04-01 08:35:04 +0000

    Basic implementation of a connection with the client

---
+++ AuthServer/Connection.cs
@@ -0,0 +1,107 @@
+﻿using System.IO.Pipelines;
+using System.Net;
+using System.Security.Cryptography;
+using Mmo.AuthServer.Crypt;
+using Mmo.AuthServer.Network;
+using Mmo.Common.Network;
+
+namespace Mmo.AuthServer;
+
+/// <summary>
+/// Connection for communicating with the client.
+/// </summary>
+public class Connection : IConnection
+{
+    /// <summary>
+    /// Size of the buffers for receiving and sending packets.
+    /// </summary>
+    public static readonly int BufferSize = 1024;
+
+    /// <summary>
+    /// Size of the packet header.
+    /// </summary>
+    private static readonly int HeaderSize = 2;
+
+    private readonly uint scrambleKey;
+    private readonly Cipher cipher;
+    private readonly IDuplexPipe pipe;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Connection"/> class.
+    /// </summary>
+    /// <param name="pipe">Pipe for communication.</param>
+    /// <param name="remoteEndPoint">Address of the peer.</param>
+    /// <param name="scrambleKey">Additional key for encryption.</param>
+    public Connection(IDuplexPipe pipe, EndPoint? remoteEndPoint = null, uint? scrambleKey = null)
+    {
+        this.scrambleKey = scrambleKey ?? (uint)RandomNumberGenerator.GetInt32(int.MaxValue);
+        this.cipher = new Cipher();
+        this.pipe = pipe;
+        this.RemoteEndPoint = remoteEndPoint;
+    }
+
+    /// <inheritdoc/>
+    public EndPoint? RemoteEndPoint { get; private set; }
+
+    /// <inheritdoc/>
+    public async Task SendAsync(IServerMessage message)
+    {
+        this.Write(message);
+        await this.pipe.Output.FlushAsync();
+    }
+
+    private void Write(IServerMessage message)
+    {
+        var buffer = this.pipe.Output.GetSpan(BufferSize);
+
+        // Write body
+        var body = buffer[HeaderSize..];
+        var bodyLength = this.WriteBody(message, body);
+
+        // Write header
+        var header = buffer[..HeaderSize];
+        this.WriteHeader(bodyLength, header);
+
+        this.pipe.Output.Advance(bodyLength + HeaderSize);
+    }
+
+    private int WriteBody(IServerMessage message, Span<byte> body)
+    {
+        var writer = new PacketWriter(body);
+        message.WriteTo(ref writer);
+
+        // Padding
+        var pad = writer.Length % CryptUtil.BlockSize;
+        if (pad != 0)
+        {
+            writer.Skip(CryptUtil.BlockSize - pad);
+        }
+
+        // Checksum
+        writer.Skip(CryptUtil.BlockSize);
+
+        // Additional encryption
+        if (message is ICryptKey)
+        {
+            writer.Skip(CryptUtil.BlockSize);
+            CryptUtil.ScrambleInit(writer.AsSpan(), this.scrambleKey);
+        }
+
+        // Encryption
+        pad = writer.Length % Cipher.BlockSize;
+        if (pad != 0)
+        {
+            writer.Skip(Cipher.BlockSize - pad);
+        }
+
+        this.cipher.Encrypt(writer.AsSpan());
+
+        return writer.Length;
+    }
+
+    private void WriteHeader(int bodyLength, Span<byte> header)
+    {
+        var writer = new PacketWriter(header);
+        writer.WriteH((short)(bodyLength + HeaderSize));
+    }
+}
---
+++ Tests/Network/AuthConnectionTest.cs
@@ -0,0 +1,102 @@
+﻿using System.IO.Pipelines;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mmo.AuthServer;
+using Mmo.AuthServer.Network;
+using Mmo.Common.Network;
+using Moq;
+
+namespace Mmo.Tests.Network;
+
+[TestClass]
+public class AuthConnectionTest
+{
+    class FakeServerMessage : ICryptKey, IServerMessage
+    {
+        public byte[] CryptKey => Array.Empty<byte>();
+
+        public void WriteTo(ref PacketWriter writer)
+        {
+            for (byte i = 1; i <= 16; i++)
+            {
+                writer.WriteC(i);
+            }
+        }
+    }
+
+    class FakePipeWriter : PipeWriter
+    {
+        public byte[] Buffer = new byte[Connection.BufferSize];
+        public int Advanced;
+        public bool Flushed;
+
+        public override void Advance(int bytes)
+        {
+            Advanced = bytes;
+        }
+
+        public override void CancelPendingFlush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Complete(Exception? exception = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask<FlushResult> FlushAsync(CancellationToken cancellationToken = default)
+        {
+            Flushed = true;
+            return new ValueTask<FlushResult>();
+        }
+
+        public override Memory<byte> GetMemory(int sizeHint = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Span<byte> GetSpan(int sizeHint = 0)
+        {
+            return Buffer;
+        }
+    }
+
+    private FakePipeWriter output;
+    private EndPoint endPoint;
+    private Connection connection;
+
+    public AuthConnectionTest()
+    {
+        this.output = new FakePipeWriter();
+        var pipe = new Mock<IDuplexPipe>();
+        pipe.Setup(x => x.Output).Returns(this.output);
+        this.endPoint = new IPEndPoint(0x7F000001, 8123);
+        this.connection = new Connection(pipe.Object, this.endPoint, 0xD5906CBC);
+    }
+
+    [TestMethod]
+    public void RemoteEndPoint()
+    {
+        // When/Then
+        Assert.AreSame(endPoint, connection.RemoteEndPoint);
+    }
+
+    [TestMethod]
+    public async Task SendAsync()
+    {
+        // Given
+        var message = new FakeServerMessage();
+
+        // When
+        await connection.SendAsync(message);
+
+        // Then
+        Assert.AreEqual(26, this.output.Advanced);
+        Assert.IsTrue(this.output.Flushed);
+        Assert.AreEqual(
+            "1A009BB6D905778ECB0ECF113FD58BFB83A5E8D00DA1A97F42CD",
+            Convert.ToHexString(this.output.Buffer[..this.output.Advanced])
+        );
+    }
+}
