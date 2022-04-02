ecac6e415672682195a514974e77d2978ea27943
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-04-02 09:17:15 +0000

    Main for the auth server (#12)

---
+++ AuthServer/Acceptor.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Connections;
+
+namespace Mmo.AuthServer;
+
+/// <summary>
+/// Accepts new connections to the server.
+/// </summary>
+public class Acceptor : ConnectionHandler
+{
+    private readonly ILogger logger;
+    private readonly IServiceProvider services;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Acceptor"/> class.
+    /// </summary>
+    /// <param name="logger">Logger for this class.</param>
+    /// <param name="services">Service container.</param>
+    public Acceptor(ILogger<Acceptor> logger, IServiceProvider services)
+    {
+        this.logger = logger;
+        this.services = services;
+    }
+
+    /// <inheritdoc/>
+    public override async Task OnConnectedAsync(ConnectionContext connection)
+    {
+        // Init client
+        var stopwatch = Stopwatch.StartNew();
+        var client = ActivatorUtilities.CreateInstance<Client>(
+            this.services,
+            new Connection(connection.Transport, connection.RemoteEndPoint));
+        await client.InitAsync();
+        stopwatch.Stop();
+        this.logger.LogTrace("Accepted new connection in {}ms", stopwatch.ElapsedMilliseconds);
+
+        // Process the client
+        await client.RunAsync();
+    }
+}
--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello World!");
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Connections;
+using Mmo.AuthServer;
+
+WebHost.
+    CreateDefaultBuilder(args).
+    UseKestrel((context, options) =>
+    {
+        var port = context.Configuration.GetValue<int>("AuthServer:Port", 2106);
+        options.ListenAnyIP(port, builder => builder.UseConnectionHandler<Acceptor>());
+    }).
+    Configure(app =>
+    {
+        // Will configure app here
+    }).
+    Build().
+    Run();
