16eca7e577eee9f8b3a03a881bce20a696524775
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-04-02 11:22:30 +0000

    Request to start GG auth (#15)

---
+++ AuthServer/Network/ClientMessages.cs
@@ -0,0 +1,36 @@
+﻿using Mmo.Common.Network;
+
+namespace Mmo.AuthServer.Network;
+
+/// <summary>
+/// Implements reading messages from the client.
+/// </summary>
+public static class ClientMessages
+{
+    /// <summary>
+    /// Read a message from the packet.
+    /// </summary>
+    /// <param name="reader">Reader with the packet.</param>
+    /// <returns>Message.</returns>
+    public static object Read(PacketReader reader)
+    {
+        var id = reader.ReadC();
+        return id switch
+        {
+            0x07 => new ClientAuthGameGuard(),
+            _ => throw new ArgumentException($"Invalid packet id (0x{id:X2})", nameof(reader)),
+        };
+    }
+}
+
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+#pragma warning disable SA1009 // Closing parenthesis should be spaced correctly
+
+/// <summary>
+/// Request to start GG auth.
+/// </summary>
+public record ClientAuthGameGuard() { }
+
+
+#pragma warning restore SA1313 // Parameter names should begin with lower-case letter
+#pragma warning restore SA1009 // Closing parenthesis should be spaced correctly
---
+++ Tests/Network/AuthClientMessagesTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mmo.AuthServer.Network;
+using Mmo.Common.Network;
+
+namespace Mmo.Tests.Network;
+
+[TestClass]
+public class AuthClientMessagesTest
+{
+    [TestMethod]
+    public void AuthGameGuard()
+    {
+        // Given
+        var buffer = Convert.FromHexString("0725C7892400000000000000000000000000000000000000");
+        var reader = new PacketReader(buffer);
+
+        // When
+        var result = ClientMessages.Read(reader);
+
+        // Then
+        if (result is ClientAuthGameGuard)
+        {
+            return;
+        }
+
+        Assert.Fail();
+    }
+}
