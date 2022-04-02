891db8989a103b7f5a90f0e809999f69a667feab
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-04-02 11:04:38 +0000

    Traffic decryption

--- Tests/Crypt/AuthCipherTest.cs
+++ Tests/Crypt/AuthCipherTest.cs
@@ -19,5 +19,19 @@ public class AuthCipherTest
         // Then
         Assert.AreEqual("569A83DF25C8816A99368A84726F4FD7", Convert.ToHexString(buffer));
     }
+
+    [TestMethod]
+    public void Decrypt()
+    {
+        // Given
+        var buffer = Convert.FromHexString("0102030405060708090A0B0C0D0E0F10");
+        var cipher = new Cipher();
+
+        // When
+        cipher.Decrypt(buffer);
+
+        // Then
+        Assert.AreEqual("741FE2984851ECB0544F3EA3F29BCD35", Convert.ToHexString(buffer));
+    }
 }
 
