422399bb05dc9f47ed612c8384a3f9d82fd06374
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-04-20 09:10:45 +0000

    Implement a basic account repository

---
+++ AuthServer/Account/IAccountRepository.cs
@@ -0,0 +1,39 @@
+﻿namespace Mmo.AuthServer.Account;
+
+/// <summary>
+/// Repository for storing accounts.
+/// </summary>
+public interface IAccountRepository
+{
+    /// <summary>
+    /// Create a new account.
+    /// </summary>
+    /// <param name="username">Username.</param>
+    /// <param name="salt">Salt.</param>
+    /// <param name="password">Password hash.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task Create(string username, byte[] salt, byte[] password);
+
+    /// <summary>
+    /// Fetch a particular account.
+    /// </summary>
+    /// <param name="username">Username.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task<AccountRecord?> Fetch(string username);
+}
+
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+
+/// <summary>
+/// Account data stored in the repository.
+/// </summary>
+/// <param name="Username">Username.</param>
+/// <param name="Salt">Salt.</param>
+/// <param name="Password">Password hash.</param>
+/// <param name="LastWorld">Last world that the account used (or zero).</param>
+/// <param name="IsBanned">Whether the account is banned.</param>
+public record AccountRecord(string Username, byte[] Salt, byte[] Password, short LastWorld, bool IsBanned)
+{
+}
+
+#pragma warning restore SA1313 // Parameter names should begin with lower-case letter
---
+++ AuthServer/Account/NpgsqlAccountRepository.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Npgsql;
+
+namespace Mmo.AuthServer.Account;
+
+/// <summary>
+/// Account repository with PostgreSQL as a backend.
+/// </summary>
+public class NpgsqlAccountRepository : IAccountRepository
+{
+    private readonly string connString;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NpgsqlAccountRepository"/> class.
+    /// </summary>
+    /// <param name="configuration">Application configuration.</param>
+    public NpgsqlAccountRepository(IConfiguration configuration)
+    {
+        this.connString = configuration.GetValue(
+            "AuthServer:AccountRepository",
+            "Host=127.0.0.1;Username=accounts;Password=changeme;Database=accounts");
+    }
+
+    /// <inheritdoc/>
+    public async Task Create(string username, byte[] salt, byte[] password)
+    {
+        await using var conn = new NpgsqlConnection(this.connString);
+        await conn.OpenAsync();
+        await conn.ExecuteAsync(
+            "INSERT INTO accounts (username, salt, password) VALUES (@username, @salt, @password)",
+            new
+            {
+                username,
+                salt = Convert.ToHexString(salt),
+                password = Convert.ToHexString(password),
+            });
+    }
+
+    /// <inheritdoc/>
+    public async Task<AccountRecord?> Fetch(string username)
+    {
+        await using var conn = new NpgsqlConnection(this.connString);
+        await conn.OpenAsync();
+        var record = await conn.QueryFirstOrDefaultAsync(
+            "SELECT salt, password, last_world, is_banned FROM accounts WHERE username = @username LIMIT 1",
+            new { username });
+        if (record == null)
+        {
+            return null;
+        }
+
+        return new AccountRecord(
+            Username: username,
+            Salt: Convert.FromHexString(record.salt),
+            Password: Convert.FromHexString(record.password),
+            LastWorld: record.last_world,
+            IsBanned: record.is_banned);
+    }
+}
