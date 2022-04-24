ab0fa88f99700e4d16e250b337689be24a2f58fe
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-04-24 15:06:17 +0000

    Find an account with given username and password (#30)

--- AuthServer/Account/AccountService.cs
+++ AuthServer/Account/AccountService.cs
@@ -22,8 +22,37 @@ public class AccountService : IAccountService
     /// <inheritdoc/>
     public Task CreateAsync(string username, string password)
     {
-        // Combine password and salt for hashing
+        // Generate salt and compute the hash
         var salt = RandomNumberGenerator.GetBytes(16);
+        var hash = ComputeHash(password, salt);
+
+        // Create the record
+        return this.repository.CreateAsync(username, salt, hash);
+    }
+
+    /// <inheritdoc/>
+    public async Task<AccountData?> FindAsync(string username, string password)
+    {
+        // Fetch the account record if it exists
+        var account = await this.repository.FetchAsync(username);
+        if (account == null)
+        {
+            return null;
+        }
+
+        // Check the password
+        var hash = ComputeHash(password, account.Salt);
+        if (!hash.AsSpan().SequenceEqual(account.Password.AsSpan()))
+        {
+            return null;
+        }
+
+        return new AccountData(account.Username, account.LastWorld, account.IsBanned);
+    }
+
+    private static byte[] ComputeHash(string password, byte[] salt)
+    {
+        // Combine password and salt for hashing
         var encoding = Encoding.UTF8;
         var length = encoding.GetByteCount(password) + salt.Length;
         var plain = new byte[length];
@@ -31,10 +60,6 @@ public class AccountService : IAccountService
         Buffer.BlockCopy(salt, 0, plain, length - salt.Length, salt.Length);
 
         // Perform hashing
-        using var sha256 = SHA256.Create();
-        var hash = sha256.ComputeHash(plain);
-
-        // Create the record
-        return this.repository.CreateAsync(username, salt, hash);
+        return SHA256.HashData(plain);
     }
 }
